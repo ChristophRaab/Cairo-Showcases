8059f99b4d09c02d8291ee94f4b7cc9b5bede2f1
Christoph Raab <christophraab@outlook.de>
2021-04-26 13:43:13 +0000

    new deep clusteriing

--- data_generation/train_representation.py
+++ data_generation/train_representation.py
@@ -21,10 +21,10 @@ from itertools import cycle
 from pytorch_metric_learning import miners, losses
 np.random.seed(0)
 torch.manual_seed(0)
-from data_generation.helpers import features_dataset,init_weights,train_resnet,create_feature_extractor,train_resnet_metric,print_learning
+from data_generation.helpers import features_dataset,init_weights,train_resnet,create_feature_extractor,train_resnet_metric,print_learning,kmeans
 from data_generation.center_loss import CenterLoss, slda
 import argparse
-from sklearn import cluster 
+
 
 
 def boolean_string(s):
@@ -38,7 +38,7 @@ def make_parser():
     parser.add_argument('--num_workers', type=int, default=0, help="Number of data loader workers")
     parser.add_argument('--bottleneck_dim', type=int, default=3, help="Bottleneck Dimension of Feature Extractor")
     parser.add_argument('--lr', type=float, default=0.001, help="Feature Extractor and Classifier LR")
-    parser.add_argument('--batch_size', type=int, default=86, help="Batch Size of Train loaders")
+    parser.add_argument('--batch_size', type=int, default=128, help="Batch Size of Train loaders")
     parser.add_argument('--num_epochs', type=int, default=100, help="Training Epochs")
     parser.add_argument('--eval_epoch', type=int, default=5, help="Evaluation Cycle")
     parser.add_argument('--mode', type=str, default="supervised", help="Network Mode" )
@@ -53,7 +53,7 @@ def train_representation(args):
 
     l = data.shape[0]
 
-    train_dataset = TensorDataset(torch.tensor(data),torch.tensor(label)) # create your datset
+    train_dataset = TensorDataset(torch.tensor(data,dtype=torch.float),torch.tensor(label)) # create your datset
 
     train_loader = DataLoader(train_dataset,shuffle=True,num_workers=args.num_workers,batch_size=args.batch_size)
     extract_loader = DataLoader(train_dataset,shuffle=False,num_workers=args.num_workers,batch_size=args.batch_size)
@@ -62,13 +62,13 @@ def train_representation(args):
     train_dataset_size = len(train_dataset)
     num_classes = len(torch.unique(train_dataset.tensors[1]))
 
-    features_extractor = nn.Sequential(create_feature_extractor(),nn.Flatten(),nn.Linear(2048,args.bottleneck_dim)).to(args.cuda)
+    features_extractor = nn.Sequential(create_feature_extractor(),nn.Flatten(),nn.Linear(2048,args.bottleneck_dim),nn.ReLU()).to(args.cuda)
     classifier = nn.Sequential(nn.Linear(args.bottleneck_dim,num_classes)).to(args.cuda)
     classifier.apply(init_weights)
     metricl = losses.NCALoss()
 
     features_extractor[-1].apply(init_weights)
-    optimizer = optim.SGD(
+    optimizer = optim.Adam(
         [{'params': features_extractor[:-1].parameters(),"lr_mult":1,'decay_mult':2},
         {'params': features_extractor[-1:].parameters(),"lr_mult":10,'decay_mult':2},
         {'params': classifier.parameters(),"lr_mult":10,'decay_mult':2}],
@@ -87,36 +87,26 @@ def train_representation(args):
                         optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet_metric(xs,ys,features_extractor,classifier,metricl,optimizer,avg_loss,avg_acc,args.cuda)
                     else:
                         optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda)
-            print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
+            best_acc = print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
             best_model = copy.deepcopy([features_extractor]) if avg_acc > best_acc else best_model
     else:
         for i in range(args.num_epochs):
-            with torch.set_grad_enabled(False):
-                features = torch.empty(0,args.bottleneck_dim,device=args.cuda)
-
-                features = features_dataset(extract_loader,features_extractor,args)
-
-                features = features.detach().cpu().numpy()
-                ky = cluster.AgglomerativeClustering(n_clusters=num_classes).fit_predict(features)
-                ky =torch.Tensor(ky).to(args.cuda).long()
-                weight = 1 / torch.unique(ky,return_counts=True)[1]
-
-            train_dataset = TensorDataset(torch.tensor(data,device=args.cuda),ky)
-            train_loader = DataLoader(train_dataset,shuffle=True,num_workers=args.num_workers,batch_size=args.batch_size)
-        
             with torch.set_grad_enabled(True):
                 avg_loss = avg_acc  = 0.0
                 for (xs,ys) in train_loader:
-                    optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda,weight)
-                print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
+                    with torch.no_grad():
+                        yc,_ = kmeans(features_extractor(xs.float().to(args.cuda)),12)
+                    optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda)
+                best_acc = print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
                 best_model = copy.deepcopy([features_extractor]) if avg_acc > best_acc else best_model
 
-                
+    print(str(best_acc))
     data = features_dataset(extract_loader,features_extractor,args)
+    # data = (data - data.mean(0)) / data.std(0)
     data_max,_ = torch.max(data,0)
     data_min,_ = torch.min(data,0)
     data = (data-data_min) / (data_max - data_min)
-
+    data = torch.nan_to_num(data,0)
     data.detach().cpu().numpy().tofile("../data_storage/resnet_"+args.mode+".bytes")
 
 
