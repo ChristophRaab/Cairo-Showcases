9303dd483085e776d1ecbe19f113953b138c6b73
Christoph Raab <christophraab@outlook.de>
2021-05-10 16:14:26 +0000

    continues development

--- data_generation/generate_dataset.py
+++ data_generation/generate_dataset.py
@@ -1,3 +1,4 @@
+from os import path
 from librosa import feature
 import pandas as pd
 import torch
@@ -18,9 +19,55 @@ import urllib
 import pandas as pd 
 import matplotlib
 import matplotlib.pyplot as plt
+import spotipy
+import argparse
+from spotipy.oauth2 import SpotifyClientCredentials
+import urllib.request
+from requests import get 
 from pathlib import Path 
-url ="https://api.spotify.com/v1/search?q="
-headers={"Authorization": "Bearer BQCjQQaLdTnMlS7fN6zIKXSIYaW6-ukBMr9pOYPQzgdlozDdjKnc4BpE6LYgZlFdVC9khodnoNC5uV7It5tqFmHSXBgqFlhESkYMli3tw65EHlZw0LmOcfDj_n-UpW5RgUNX854RHM5v-xM", "Accept": "application/json", "Content-Type": "application/json" }
+
+
+def create_sprite(data):
+    """
+    Tile images into sprite image. 
+    Add any necessary padding
+    """
+    # For B&W or greyscale images
+    if len(data.shape) == 3:
+        data = np.tile(data[...,np.newaxis], (1,1,1,3))
+
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = ((0, n ** 2 - data.shape[0]), (0, 0), (0, 0), (0, 0))
+    data = np.pad(data, padding, mode='constant',
+            constant_values=0)
+    # Tile images into sprite
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3, 4))
+    # print(data.shape) => (n, image_height, n, image_width, 3)
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    # print(data.shape) => (n * image_height, n * image_width, 3) 
+    return data
+
+def sprite():
+    labels = []
+    metadata = []
+    images = []
+    for file in glob.glob("preview/mp3/"+ '/**/*.mp3', recursive=True):
+
+        tag = eyed3.load(file).tag
+        artist = tag.artist.split("/")[0]
+        metadata.append([artist])
+        labels.append(artist)
+        input_img = cv2.imread("artist_image/"+artist.split(" ")[-1].lower()+".jpg")
+        input_img_resize = cv2.resize(input_img, (32,32)) 
+        images.append(input_img_resize)
+    img_data = np.array(images)
+    sprite = create_sprite(img_data)
+    cv2.imwrite("sprite.png", sprite)
+
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
 
 def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
   fig, axs = plt.subplots(1, 1)
@@ -33,13 +80,13 @@ def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=No
   fig.colorbar(im, ax=axs)
   plt.show(block=False)
 
-def get_embeded_url(l): #l df["Artist"],df["Songname"])
-    song = l.replace("\"","")
-    song = urllib.parse.quote_plus(song)
-    req = requests.get(url+song+"&type=track", headers=headers)
-    data = req.json()
-    meta = data["tracks"]["items"][0]["external_urls"]["spotify"]
-    return meta
+def download_track(url, file_name):
+    # open in binary mode
+    with open(file_name, "wb") as file:
+        # get request
+        response = get(url)
+        # write to file
+        file.write(response.content)
 
 def spec_to_image(spec, eps=1e-6):
     mean = spec.mean()
@@ -50,8 +97,7 @@ def spec_to_image(spec, eps=1e-6):
     spec_scaled = spec_scaled.astype(np.uint8)
     return spec_scaled
 
-def get_melspectrogram_db(file_path, sr=None, n_fft=8192, hop_length=512, n_mels=128, fmin=20, fmax=8300, top_db=80):
-    wav,sr = librosa.load(file_path,sr=sr)
+def get_melspectrogram_db(wav, sr, n_fft=8192, hop_length=512, n_mels=128, fmin=20, fmax=8300, top_db=80):
     if wav.shape[0]<5*sr:
         wav=np.pad(wav,int(np.ceil((5*sr-wav.shape[0])/2)),mode='reflect')
     else:
@@ -61,31 +107,80 @@ def get_melspectrogram_db(file_path, sr=None, n_fft=8192, hop_length=512, n_mels
     spec_db=librosa.power_to_db(spec,top_db=top_db)
     return spec_db
 
-def feature_exctraction(path):
+def song_meta_data(track_obj,artist_name,album_name):
+    
+    track = track_obj
+    artists = [ n["name"] for n in track["artists"] if n["name"] == artist_name]
 
-    data_raw = []
-    data = []
-    labels = []
-    metadata = []
+    if len(artists) > 0:
+        artist = artists[0].replace("/"," ")
+        name = track["name"].replace("/"," ")
+        album = track["album"]["name"].replace("/"," ") if album_name == None else album_name
+        preview_url = track["preview_url"]
+        external_url = track["external_urls"]["spotify"]
+        return [artist,name,album,external_url],preview_url
+    else:
+        return (None,None)
 
-    for file in glob.glob(path+ '/**/*.mp3', recursive=True):
-        song_meta = read_song_metadata(file)
-        metadata.append(song_meta)
-        labels.append(song_meta[0])
+def extract_playlist(id,name,ptype,sp):
+    ply_raw = []
+    ply_data = []
+    ply_label = []
+    ply_metadata =[]
+    
+    if ptype == "playlist":
+        results = sp.playlist(id) 
+        album_name = None
+    else:
+        results = sp.album(id)
+        album_name = results["name"] 
 
-        music = librosa.load(file,offset=45.0, duration=5.0,sr=None)
-        data_raw.append(music[0])
-        song_spec = spec_to_image(get_melspectrogram_db(file))
-        data.append(song_spec[np.newaxis,...])
-        subdir = file.split("mp3/")[0]+"images/"+song_meta[0]
-        img_path = subdir+"/"+file.split("/")[-1].split(".mp3")[0]+".png"
-        Path(subdir).mkdir(parents=True, exist_ok=True)
-        plt.imshow(song_spec)
-        plt.savefig(img_path)
+    for track_obj in results["tracks"]["items"]:
+        if ptype == "playlist":
+            song_meta,download_url = song_meta_data(track_obj["track"],name,album_name)
+        else:
+            song_meta,download_url = song_meta_data(track_obj,name,album_name)
+
+        if download_url != None and song_meta != None:
+            ply_metadata.append(song_meta)
+            ply_label.append(song_meta[0])
+
+            file = "preview/mp3/"+song_meta[0]+"/"+song_meta[1]+".mp3"
+            Path("preview/mp3/"+song_meta[0]).mkdir(parents=True, exist_ok=True)
+
+            if not os.path.isfile(file):
+                download_track(download_url,file)
+
+            music,sr = librosa.load(file,offset=0.0, duration=5.0,sr=None)
+            ply_raw.append(music[0])
+            song_spec = spec_to_image(get_melspectrogram_db(music,sr))
+            ply_data.append(song_spec[np.newaxis,...])
+
+            subdir = file.split("mp3/")[0]+"images/"+song_meta[0]
+            Path(subdir).mkdir(parents=True, exist_ok=True)
+
+            img_path = subdir+"/"+file.split("/")[-1].split(".mp3")[0]+".png"
+            
+            plt.imshow(song_spec)
+            plt.savefig(img_path)
+
+    return ply_raw, ply_data,ply_label,ply_metadata
+
+def feature_extraction(path_playlists,sp,args):
+
+    data_raw =[]
+    data=[]
+    labels=[]
+    metadata =[]
+
+    playlists = pd.read_csv(path_playlists,sep=",",header=None,index_col=False)
+
+    for i,name,ptype,url in playlists.itertuples():
+        ply_raw, ply_data,ply_label,ply_metadata = extract_playlist(url,name,ptype,sp)
+        data_raw.append(ply_raw),data.append(ply_data),labels.append(ply_label),metadata.append(ply_metadata)
 
     le = LabelEncoder()
     le.fit(labels)
-    a = le.classes_
     binarized_labels = le.transform(labels)
     data = np.array(data)
     data_raw = np.array(data_raw)
@@ -96,67 +191,18 @@ def feature_exctraction(path):
     df = pd.DataFrame(metadata)
     df.to_csv(header=["Artist","Songname","Album","EmbUrl"],sep="\t",index=False,path_or_buf='metadata.tsv')
 
-def read_song_metadata(f):
-    tag = eyed3.load(f).tag
-    artist = tag.artist.split("/")[0]
-    url = get_embeded_url(artist+" "+tag.title.replace("\"",""))
-    return [artist,tag.title.replace("\"",""),tag.album.replace("\"",""),url]
-
-def create_tensorboard_metadata(path):
-    metadata = []
-
-    for file in glob.glob(path+ '/**/*.mp3', recursive=True):
-        print(file)
-        metadata.append(read_song_metadata(file))
-
-    df = pd.DataFrame(metadata)
-    df.to_csv(header=["Artist","Songname","Album","EmbUrl"],sep="\t",index=False,path_or_buf='metadata.tsv')
-
-def create_sprite(data):
-    """
-    Tile images into sprite image. 
-    Add any necessary padding
-    """
-    
-    # For B&W or greyscale images
-    if len(data.shape) == 3:
-        data = np.tile(data[...,np.newaxis], (1,1,1,3))
-
-    n = int(np.ceil(np.sqrt(data.shape[0])))
-    padding = ((0, n ** 2 - data.shape[0]), (0, 0), (0, 0), (0, 0))
-    data = np.pad(data, padding, mode='constant',
-            constant_values=0)
-    
-    # Tile images into sprite
-    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3, 4))
-    # print(data.shape) => (n, image_height, n, image_width, 3)
-    
-    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
-    # print(data.shape) => (n * image_height, n * image_width, 3) 
-    return data
-
-def sprite(path):
-
-    labels = []
-    metadata = []
-    images = []
-    for file in glob.glob(path+"spotify/mp3/"+ '/**/*.mp3', recursive=True):
-
-        tag = eyed3.load(file).tag
-        artist = tag.artist.split("/")[0]
-        metadata.append([artist])
-        labels.append(artist)
-        input_img = cv2.imread(path+"artist_image/"+artist.split(" ")[-1].lower()+".jpg")
-        input_img_resize = cv2.resize(input_img, (32,32)) 
-        images.append(input_img_resize)
-    img_data = np.array(images)
-    sprite = create_sprite(img_data)
-    cv2.imwrite("sprite.png", sprite)
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Feature Extractor Music Mozart')
+    parser.add_argument('--id',  default="", type=str, help="Client")
+    parser.add_argument('--secret',  default="", type=str, help="Client")
+    parser.add_argument('--download',  default=True, type=boolean_string, help="Download preview mp3")
+    args =  parser.parse_args()
 
-    feature_exctraction('/home/raabc/Jukebox/data_generation/spotify')
-#   create_tensorboard_metadata('/home/bix/Christoph/owncloud/mozartai/jukebox/data_generation/spotify/mp3/')
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=args.id,
+                                                           client_secret=args.secret))
+    sp.country_codes = ["DE"]
+    feature_extraction("playlists.csv",sp,args)
+  # sprite('/home/raabc/jukebox/data_generation/')
 
   # from sklearn.decomposition import PCA
   # from sklearn.preprocessing import StandardScaler
@@ -166,4 +212,3 @@ if __name__ == '__main__':
   # data = PCA(50).fit_transform(data)
   # data.tofile("data_generation/pca.bytes")
 
-  # sprite('/home/bix/Christoph/owncloud/mozartai/jukebox/data_generation/')
--- data_generation/train_representation.py
+++ data_generation/train_representation.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division
 from numpy.lib.utils import source
 import sys
+from sklearn.utils import validation
+from torch.functional import Tensor
 sys.path.append("../../Jukebox/")
 sys.path.append("../Jukebox/")
 from torch.optim import optimizer
@@ -21,9 +23,11 @@ from itertools import cycle
 from pytorch_metric_learning import miners, losses
 np.random.seed(0)
 torch.manual_seed(0)
-from data_generation.helpers import features_dataset,init_weights,train_resnet,create_feature_extractor,train_resnet_metric,print_learning,kmeans
+from data_generation.helpers import eval_resnet, features_dataset,init_weights,train_resnet,create_feature_extractor,train_resnet_metric,print_learning,kmeans, validation_epoch
 from data_generation.center_loss import CenterLoss, slda
 import argparse
+from sklearn import cluster
+
 
 
 
@@ -34,7 +38,7 @@ def boolean_string(s):
 
 def make_parser():
     parser = argparse.ArgumentParser(description='Mozart Feature Generation Adversarial')
-    parser.add_argument('--cuda', type=str, default='cuda:0', help="GPU Device ID")
+    parser.add_argument('--cuda', type=str, default='cuda:3', help="GPU Device ID")
     parser.add_argument('--num_workers', type=int, default=0, help="Number of data loader workers")
     parser.add_argument('--bottleneck_dim', type=int, default=3, help="Bottleneck Dimension of Feature Extractor")
     parser.add_argument('--lr', type=float, default=0.001, help="Feature Extractor and Classifier LR")
@@ -52,26 +56,35 @@ def train_representation(args):
     label = np.load(args.path+'/classicalmusic_labels.npy')
 
     l = data.shape[0]
+    ratio = 0.9
+    indices = np.random.permutation(data.shape[0])
+    sidx, tidx = indices[:int(data.shape[0]*ratio)], indices[int(data.shape[0]*ratio):]
+    train_data, validation_data = torch.tensor(data[sidx,:]), torch.tensor(data[tidx,:])
+    train_label, vallidation_label = torch.tensor(label[sidx]), torch.tensor(label[tidx])
 
-    train_dataset = TensorDataset(torch.tensor(data,dtype=torch.float),torch.tensor(label)) # create your datset
+
+    train_dataset = TensorDataset(train_data,train_label) # create your datset
+    validation_dataset = TensorDataset(validation_data,vallidation_label) # create your datset
+    complete_dataset = TensorDataset(torch.tensor(data),torch.tensor(label))
 
     train_loader = DataLoader(train_dataset,shuffle=True,num_workers=args.num_workers,batch_size=args.batch_size)
-    extract_loader = DataLoader(train_dataset,shuffle=False,num_workers=args.num_workers,batch_size=args.batch_size)
+    validation_loader = DataLoader(validation_dataset,shuffle=True,num_workers=args.num_workers,batch_size=args.batch_size)
+    extract_loader = DataLoader(complete_dataset,shuffle=False,num_workers=args.num_workers,batch_size=args.batch_size)
 
-    train_loader_size,extract_loader_size = len(train_loader),len(extract_loader)
-    train_dataset_size = len(train_dataset)
+    train_loader_size,extract_loader_size,validation_loader_size = len(train_loader),len(extract_loader),len(validation_loader)
+    train_dataset_size,validation_dataset_size = len(train_dataset),len(validation_dataset)
     num_classes = len(torch.unique(train_dataset.tensors[1]))
 
-    features_extractor = nn.Sequential(create_feature_extractor(),nn.Flatten(),nn.Linear(2048,args.bottleneck_dim),nn.ReLU()).to(args.cuda)
+    features_extractor = nn.Sequential(create_feature_extractor(),nn.Flatten(),nn.Linear(2048,args.bottleneck_dim),nn.BatchNorm1d(args.bottleneck_dim)).to(args.cuda)
     classifier = nn.Sequential(nn.Linear(args.bottleneck_dim,num_classes)).to(args.cuda)
     classifier.apply(init_weights)
     metricl = losses.NCALoss()
 
-    features_extractor[-1].apply(init_weights)
+    features_extractor[-2].apply(init_weights)
     optimizer = optim.Adam(
-        [{'params': features_extractor[:-1].parameters(),"lr_mult":1,'decay_mult':2},
-        {'params': features_extractor[-1:].parameters(),"lr_mult":10,'decay_mult':2},
-        {'params': classifier.parameters(),"lr_mult":10,'decay_mult':2}],
+        [{'params': features_extractor[:-2].parameters(),"lr_mult":1,'decay_mult':2},
+        {'params': features_extractor[-2:].parameters(),'lr':args.lr*10,"lr_mult":10,'decay_mult':2},
+        {'params': classifier.parameters(),'lr':args.lr*10,"lr_mult":10,'decay_mult':2}],
         lr=args.lr,weight_decay=0.0005)
 
     features_extractor.train(),classifier.train()
@@ -82,31 +95,38 @@ def train_representation(args):
             with torch.set_grad_enabled(True):
                 avg_loss = avg_acc  = 0.0
                 for (xs,ys) in train_loader:
-                    
-                    if args.mode == "metric":
-                        optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet_metric(xs,ys,features_extractor,classifier,metricl,optimizer,avg_loss,avg_acc,args.cuda)
-                    else:
-                        optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda)
-            best_acc = print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
-            best_model = copy.deepcopy([features_extractor]) if avg_acc > best_acc else best_model
+                    optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,i,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda)
+            if i % 5 == 0:
+                best_acc,best_model = validation_epoch(validation_loader,features_extractor,classifier,i,validation_dataset_size,validation_loader_size,best_model,best_acc,args)
     else:
         for i in range(args.num_epochs):
+            with torch.set_grad_enabled(False):
+                features = torch.empty(0,args.bottleneck_dim,device=args.cuda)
+                features = features_dataset(extract_loader,features_extractor,args)
+
+                # ky,weight = kmeans(features,num_classes,50)
+                features = features.detach().cpu().numpy()
+                ky = cluster.KMeans(n_clusters=num_classes).fit_predict(features)
+                ky =torch.Tensor(ky).to(args.cuda).long()
+                weight = 1 / torch.unique(ky,return_counts=True)[1]
+
+            train_dataset = TensorDataset(torch.tensor(data,device=args.cuda),ky)
+            train_loader = DataLoader(train_dataset,shuffle=True,num_workers=args.num_workers,batch_size=args.batch_size)
+
             with torch.set_grad_enabled(True):
                 avg_loss = avg_acc  = 0.0
                 for (xs,ys) in train_loader:
-                    with torch.no_grad():
-                        yc,_ = kmeans(features_extractor(xs.float().to(args.cuda)),12)
-                    optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda)
-                best_acc = print_learning(i,avg_acc,train_dataset_size,avg_loss,train_loader_size,best_acc)
-                best_model = copy.deepcopy([features_extractor]) if avg_acc > best_acc else best_model
-
-    print(str(best_acc))
+                    optimizer,features_extractor,loss_func,avg_loss,avg_acc= train_resnet(xs,ys,i,features_extractor,classifier,optimizer,avg_loss,avg_acc,args.cuda,weight)
+            if i % 5 == 0:
+                best_acc,best_model = validation_epoch(validation_loader,features_extractor,classifier,i,validation_dataset_size,validation_loader_size,best_model,best_acc,args)
+    
+    print("Best:"+str(best_acc))
     data = features_dataset(extract_loader,features_extractor,args)
     # data = (data - data.mean(0)) / data.std(0)
     data_max,_ = torch.max(data,0)
     data_min,_ = torch.min(data,0)
     data = (data-data_min) / (data_max - data_min)
-    data = torch.nan_to_num(data,0)
+    # data = torch.nan_to_num(data,0)
     data.detach().cpu().numpy().tofile("../data_storage/resnet_"+args.mode+".bytes")
 
 
@@ -114,3 +134,7 @@ if __name__ == "__main__":
 
     args = make_parser()
     train_representation(args)
+
+#train_representation.py --mode unsupervised --cuda cuda:1 --num_epochs 2000 --batch_size 256 
+#train_representation.py --mode supervised --cuda cuda:1 --num_epochs 300 --batch_size 64 
+#train_representation.py --mode metric --cuda cuda:1 --num_epochs 300 --batch_size 64 
